6cfe50452d343bb59d598efac72b8d435c29aedf
Miguel Varona <mavaronad@gmail.com>
2018-02-09 13:18:06 -0400

    Se añade body-parser. Mitad del video n° 16

--- Node/Proyecto/app.js
+++ Node/Proyecto/app.js
@@ -1,7 +1,10 @@
 var express = require("express");
-
+var bodyParser = require("body-parser");
 var app = express();
 
+app.use("/public", express.static('public'));
+app.use(bodyParser.json()); //para peticiones application/json
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "jade");
 
 app.get("/",function(req,res){
