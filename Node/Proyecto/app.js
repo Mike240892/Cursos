0aa1324582bdc239fa00caf08e33d3336622f7e7
Miguel Varona <mavaronad@gmail.com>
2018-03-02 17:01:04 -0400

    Algunos pocos avances en el curso 02/03/2018 5:00 p.m.

--- Node/Proyecto/app.js
+++ Node/Proyecto/app.js
@@ -1,24 +1,25 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var User = require("./models/user").User;
 var app = express();
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
+//var mongoose = require("mongoose");
+//var Schema = mongoose.Schema;
 
-mongoose.connect("mongodb://localhost/fotos");
 
-var userSchemaJSON = {
-	email:String,
-	password: String
-};
+//mongoose.connect("mongodb://localhost/fotos");
 
-var user_schema = new Schema(userSchemaJSON);
+//var userSchemaJSON = {
+//	email:String,
+//	password: String
+//};
 
-var User = mongoose.model("User",user_schema);
+//var user_schema = new Schema(userSchemaJSON);
+
+//var User = mongoose.model("User",user_schema);
 
 app.use("/public", express.static('public'));
 
 app.use(bodyParser.json()); //para peticiones application/json
-
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "jade");
@@ -31,6 +32,7 @@ app.get("/login",function(req,res){
 	User.find(function(err,doc){
 		console.log(doc);
 		res.render("login");
+		console.log(doc);
 	});	
 });
 
